e9b35337bf730eeb7cd77e07425a0ef2419db3af
Saurabh <mail.saurabhmishra.in@gmail.com>
2021-01-18 00:02:39 +0530

    v2

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import *
 from connect import *
+import datetime
 
 app = Flask(__name__)
 
@@ -12,20 +13,43 @@ def connection():
 def sample_api():
     return "sample api showing that it worked"
 
-@app.route("/scanned",methods=["POST"])
-def scanned():
+def cal_fine(start):
+    duration = str(datetime.date.today()-start)[0:3]
+    return str(int(duration)*2)
+
+@app.route("/book_info",methods=["POST"])
+def book_info():
     collection = connection()
-    data = request.get_json(force = True)
-    id = data["id"]
-    print(data,"\n",id)
+    rec_data = request.get_json(force=True)
+    for i in collection.find({"id":rec_data["id"]}):
+        ins_data = {
+            "id": i["id"],
+            "student_id": i["student_id"],
+            "author": i["author"],
+            "fine_rem": cal_fine(i["issue_date"]),
+        }
+        return ins_data
+    return "No match found"
+
+@app.route("/issue",methods=["POST"])
+def issue():
+    collection = connection()
+    rec_data = request.get_json(force = True)
+    print(rec_data)
+    id = rec_data["id"]
+    ins_data = {
+                "student_name":rec_data["student_name"],
+                "student_id":rec_data["student_id"],
+                "issue_date":datetime.date.today(),
+                "fine_rem":0,
+                "issued_bool":"1"
+                }
     for i in collection.find({"id" : id}):
-        if int(i["issued_bool"]):
-            return "This book is already issued to {0} and the fine amount is {1}".format(i["student_name"],i["fine_rem"])
-        else:
-            return "This book is not issued"
-    return "None"
+        collection.update_one({"id":rec_data["id"]},{"$set":ins_data})
+        return "Done"
+    return "This book doesn't exist in database"
 
 
 if __name__ == "__main__":
-    app.run(host="192.168.1.7" , port="8900")
+    app.run(host="192.168.146.182" , port="8900")
 
